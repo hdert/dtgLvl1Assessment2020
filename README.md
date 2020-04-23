65a4d0316b35306e66916cf3dba50bcf4010e5f0
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-04-23 18:22:54 +1200

    do lots of work

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dtgLvl1Assessment2020
 code, planning, and evidence for the Digital Technologies Level one assessment for 2020.
+Trello: https://trello.com/b/LG6nuAm4/dtglvl1assessment2020
---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*
+main.js © Copyright 2020 Justin Muirhead under the BSD 3-Clause License
+code snippets taken from: https://w3schools.com/
+and: https://github.com/jhay0112/jhay0112.github.io
+*/
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function exitLoadingScreen() {
+    loadingScreen = document.getElementById("loadingScreen");
+    loadingSpinner = document.getElementById("spinner");
+    loadingLogo = document.getElementById("logo");
+
+    loadingLogo.style.animation = "fadeOut 0.5s 1s forwards ease";
+    loadingSpinner.style.animation = "spin 1s infinite linear, fadeOut 2s forwards";
+    loadingScreen.style.animation = "fadeOut 1.3s 1.5s forwards ease-out";
+    await sleep(1200);
+    document.body.style.overflowY = "auto";
+    await sleep(1000);
+    loadingScreen.style.animation = "slideOutBottom 0s forwards"
+        /* this makes it so that the scrollbar doesn't show until the loading animation is near complete */
+}
