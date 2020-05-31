1a415a5265f4c800883a613214c1bc8cfa118309
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2020-05-31 13:50:51 +1200

    prep website

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
+/*
+main.ts © Copyright 2020 Justin Muirhead under the BSD 3-Clause License
+code snippets taken from: https://www.w3schools.com/
+and: https://github.com/jhay0112/jhay0112.github.io/
+go visit: https://jordanhay.tk/
+*/
+// SPDX-License-Identifier: BSD-3-Clause
 "use strict";
 const WRAPPER = document.getElementById("wrapper");
 const NAVBAR = document.getElementById("navbar");
 const PROGRESSBAR = document.getElementById("progressBar");
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
+    /* A function that sleeps for a time in ms */
 }
 async function exitLoadingScreen() {
     document.getElementById("logo").style.animation =
@@ -14,13 +22,18 @@ async function exitLoadingScreen() {
         "slideOutBottom 1.3s 1.5s forwards ease-out";
     await sleep(1200);
     WRAPPER.style.overflowY = "auto";
+    /* This makes it so that the scrollbar doesn't show until the loading animation is near complete. */
 }
+/* Menu */
 function openMenu() {
     document.getElementById("menu").style.width = "100vw";
 }
 function closeMenu() {
     document.getElementById("menu").style.width = "0vw";
 }
+/* These funcions open and close the menu with an animation which is done by
+setting a transition time in the css. */
+/* Scroll functions */
 function goToTop() {
     WRAPPER.scrollTop = 0;
 }
@@ -41,6 +54,8 @@ var navbarHide = (function () {
         PREVSCROLLPOS = currentScrollPos;
     };
 })();
+/* This function hides and unhides the navbar based on whether the user has scrolled up
+or scrolled down since the last function call */
 function scrollIndicatorUpdate() {
     PROGRESSBAR.style.width =
         (WRAPPER.scrollTop /
@@ -48,6 +63,8 @@ function scrollIndicatorUpdate() {
             100 +
             "%";
 }
+/* This function updates the progressBar at the bottom of the navbar everytime the user
+scrolls proportional to where they are on the page in the x-axis at the moment. */
 function backToTopButtonUpdate() {
     if (WRAPPER.scrollTop > 250) {
         document.getElementById("backToTopButton").style.animation = "fadeIn ease 0.3s";
@@ -56,6 +73,8 @@ function backToTopButtonUpdate() {
         document.getElementById("backToTopButton").style.animation = "fadeOut ease 0.3s forwards";
     }
 }
+/* This function checks if the user has scrolled past a certain point, if they have
+it shows the backToTopButton and if they haven't it hides it. */
 WRAPPER.onscroll = function () {
     backToTopButtonUpdate();
     navbarHide();
--- js/main.ts
+++ js/main.ts
@@ -15,6 +15,7 @@ const PROGRESSBAR = document.getElementById("progressBar") as HTMLDivElement;
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
+  /* A function that sleeps for a time in ms */
 }
 
 async function exitLoadingScreen() {
@@ -38,6 +39,9 @@ function openMenu() {
 function closeMenu() {
   (document.getElementById("menu") as HTMLDivElement).style.width = "0vw";
 }
+
+/* These funcions open and close the menu with an animation which is done by
+setting a transition time in the css. */
 
 /* Scroll functions */
 
@@ -63,6 +67,8 @@ var navbarHide = (function () {
     PREVSCROLLPOS = currentScrollPos;
   };
 })();
+/* This function hides and unhides the navbar based on whether the user has scrolled up
+or scrolled down since the last function call */
 
 function scrollIndicatorUpdate() {
   PROGRESSBAR.style.width =
@@ -71,6 +77,8 @@ function scrollIndicatorUpdate() {
       100 +
     "%";
 }
+/* This function updates the progressBar at the bottom of the navbar everytime the user
+scrolls proportional to where they are on the page in the x-axis at the moment. */
 
 function backToTopButtonUpdate() {
   if (WRAPPER.scrollTop > 250) {
@@ -83,9 +91,13 @@ function backToTopButtonUpdate() {
     ) as HTMLButtonElement).style.animation = "fadeOut ease 0.3s forwards";
   }
 }
+/* This function checks if the user has scrolled past a certain point, if they have
+it shows the backToTopButton and if they haven't it hides it. */
 
 WRAPPER.onscroll = function () {
   backToTopButtonUpdate();
   navbarHide();
   scrollIndicatorUpdate();
 };
+/* This attaches an onScroll event to the scroll wrapper and calls the scroll events
+when it is triggered. */
